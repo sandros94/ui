38d63a263eb8de105ad7162bf7218772bd241afc
Sandros94 <sandro.circi@digitoolmedia.com>
2023-11-06 17:00:57 +0100

    init: project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { createResolver } from 'nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  alias: { '@s94-ui': resolve('./') },
+  modules: ['@nuxt/ui'],
+  components: [
+    { path: '@s94-ui/components', prefix: 'S', pathPrefix: false },
+  ],
+  devtools: { enabled: true }
+})
