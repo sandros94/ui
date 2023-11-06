88884bff3e2e60d2fd9cd0db0c6b0e6e6019c1b0
Sandros94 <sandro.circi@digitoolmedia.com>
2023-11-06 17:05:33 +0100

    refactor: more in line with nuxt conventions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ const { resolve } = createResolver(import.meta.url)
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  alias: { '@s94-ui': resolve('./') },
+  alias: { '#s94-ui': resolve('./') },
   modules: ['@nuxt/ui'],
   components: [
-    { path: '@s94-ui/components', prefix: 'S', pathPrefix: false },
+    { path: '#s94-ui/components', prefix: 'S', pathPrefix: false },
   ],
   devtools: { enabled: true }
 })
