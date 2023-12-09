66394bd145c15b6be5ec0c81216bdae1c3f7c0c5
Sandros94 <sandro.circi@digitoolmedia.com>
2023-12-09 03:00:20 +0100

    feat(SMain): padded screen centering

--- .playground/pages/index.vue
+++ .playground/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full min-h-[inherit] grid place-items-center">
-    <div>
+  <div class="h-full w-fit mx-auto">
+    <div v-for="i of 1">
       <!-- Component to be tested -->
       <SHero headline="Welcome to s94/ui" subhead="This is a custom Nuxt layer that extends Nuxt UI's components." :cta="[{label: 'Get yours now', trailingIcon: 'i-ph-arrow-right'}, {label: 'More info', color: 'gray', trailingIcon: 'i-ph-question'}]" horizontal>
         <img src="https://cms.thetouchofficial.com/assets/071702b5-1a55-45d2-b81a-6ddd8d3fa707?withoutEnlargement=true&width=1920&format=webp&fit=outside" alt="Hero image">
--- components/layout/Main.vue
+++ components/layout/Main.vue
@@ -1,18 +1,29 @@
 <template>
   <main :class="ui.wrapper" v-bind="attrs">
     <slot />
+    <!-- the following div is just to offset the `justify-around` and compensate for the header -->
+    <div v-if="padded" />
   </main>
 </template>
 
 <script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
+
 const config = {
   wrapper: 'min-h-[calc(100svh-var(--header-height))] @container/main'
 }
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   ui?: Partial<typeof config>
   class?: any
-}>()
+  padded?: boolean
+}>(), {
+  ui: () => ({}),
+  class: undefined,
+  padded: true
+})
+
+config.wrapper = twMerge(config.wrapper, props.padded ? 'flex flex-col justify-around' : '')
 
 const { ui, attrs } = useUI('s94.main', toRef(props, 'ui'), config, toRef(props, 'class'))
 </script>
