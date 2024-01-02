7c9d7917598aef06761d4d18b8d5d85d23233983
Sandros94 <sandro.circi@digitoolmedia.com>
2024-01-02 16:10:51 +0100

    chore: update demo

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -2,12 +2,19 @@
   <div class="mx-auto h-full w-full">
     <div>
       <!-- Component to be tested -->
-      <span class="inline-flex w-full justify-center gap-x-4">
-        <p>Show Link</p>
-        <UToggle v-model="isVisible" />
-      </span>
       <SHero
-        :cta="[{label: 'Install it now', trailingIcon: 'i-ph-arrow-right'}, {label: 'More info', color: 'gray', trailingIcon: 'i-ph-question'}]"
+        :cta="[
+          {
+            label: 'Install it now',
+            trailingIcon: 'i-ph-arrow-right',
+          },
+          {
+            label: isVisible ? 'Less info' : 'More info',
+            color: 'gray',
+            trailingIcon: 'i-ph-question',
+            click: () => isVisible = !isVisible,
+          }
+        ]"
         headline="Welcome to s94/ui"
         horizontal
         subhead="This is a custom Nuxt layer that extends Nuxt UI's components."
