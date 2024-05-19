787b8471f66b2edc033f8e92a3020d2483ce7e9b
Sandros94 <sandro.circi@digitoolmedia.com>
2024-05-19 20:20:00 +0200

    style(SHeadline): span mx-auto

--- themes/headline-span.ts
+++ themes/headline-span.ts
@@ -1,5 +1,5 @@
 import { type TV } from 'tailwind-variants'
 
 export default {
-  base: 'leading-none',
+  base: 'mx-auto leading-none',
 } satisfies Parameters<TV>[0]
--- themes/headline.ts
+++ themes/headline.ts
@@ -3,6 +3,6 @@ import { type TV } from 'tailwind-variants'
 export default {
   slots: {
     root: 'mx-auto font-bold',
-    span: 'leading-none',
+    span: 'mx-auto leading-none',
   },
 } satisfies Parameters<TV>[0]
