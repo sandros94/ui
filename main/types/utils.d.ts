2aa14ea879897febb71d80d6b01dc05d8da754e1
Sandros94 <sandro.circi@digitoolmedia.com>
2024-06-13 14:03:28 +0200

    chore: remove leftovers

--- main/types/utils.d.ts
+++ main/types/utils.d.ts
@@ -1,11 +1,5 @@
 export type Strategy = 'merge' | 'override'
 
-export type NestedKeyOf<ObjectType extends Record<string, any>> = {
-  [Key in keyof ObjectType]: ObjectType[Key] extends Record<string, any>
-    ? NestedKeyOf<ObjectType[Key]>
-    : Key
-}[keyof ObjectType]
-
 export type DeepPartial<T> = Partial<{
   [P in keyof T]: { [key: string]: object | string } | DeepPartial<T[P]>
 }>
@@ -23,10 +17,3 @@ export type ExtractDeepKey<T, Path extends string[]> = DeepKey<T, Path> extends 
   ? Result extends Record<string, any> ? keyof Result : never
   : never
 
-export type ExtractDeepObject<T, Path extends string[]> = DeepKey<T, Path> extends infer Result
-  ? Result extends Record<string, any> ? Result : never
-  : never
-
-export type Optional<T> = {
-  [P in keyof T]?: T[P] extends object ? Optional<T[P]> : T[P]
-}
